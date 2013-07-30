bf37f33fb26701ba100c0bde2704e51bc5fc03bf
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-29 21:56:52 -0500

    polished

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,7 +11,7 @@ get '/sessions/new' do
 end
 
 post '/sessions' do
-  @user = User.authenticate(params)
+  @user = User.authenticate(params[:email],params[:password])
   if @user
     session[:user_id] = @user.id
     redirect to ('/')
@@ -20,7 +20,7 @@ post '/sessions' do
   end
 end
 
-delete '/sessions/:id' do
+get '/sessions/:id' do
   session.clear
   redirect to '/'
 end
